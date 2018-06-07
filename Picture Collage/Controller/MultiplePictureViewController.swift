a3ee78c1716c6ddf6ca9b63e55be5c81b48622d1
Heema Rajkarnikar <heemar@hotmail.com>
2018-06-07 05:55:08 -0400

    fixed image merging issue

--- Picture Collage/Controller/MultiplePictureViewController.swift
+++ Picture Collage/Controller/MultiplePictureViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 import OpalImagePicker
 
 class MultiplePictureViewController: UIViewController {
+    
+    let imagePicker = OpalImagePickerController()
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imagePicker.imagePickerDelegate = self
+        imagePicker.maximumSelectionsAllowed = 2
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +30,8 @@ class MultiplePictureViewController: UIViewController {
     }
     
     @IBAction func choosePicturePressed(_ sender: UIButton) {
+        
+        present(imagePicker, animated: true, completion: nil)
         
     }
     
@@ -36,6 +45,46 @@ class MultiplePictureViewController: UIViewController {
     }
     */
 
+}
+
+extension MultiplePictureViewController: OpalImagePickerControllerDelegate {
+    func imagePickerDidCancel(_ picker: OpalImagePickerController) {
+        //Cancel action?
+    }
+    
+    func imagePicker(_ picker: OpalImagePickerController, didFinishPickingImages images: [UIImage]) {
+        var width : CGFloat = 0
+        var height : CGFloat = 0
+        var y: CGFloat = 0
+        var imageHeight : CGFloat = 0
+        
+        //Save Images, update UI
+        for image in images{
+            if width < image.size.width {
+                width = image.size.width
+            }
+            height = height + image.size.height
+        }
+        
+        let size = CGSize(width: width, height: height)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        for image in images{
+            imageHeight = y + image.size.height
+            image.draw(in: CGRect(x:0, y: y, width: width, height: imageHeight))
+            y = y + image.size.height
+        }
+        
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        UIImageWriteToSavedPhotosAlbum(newImage, nil, nil, nil)
+
+        
+        //Dismiss Controller
+        presentedViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
 
